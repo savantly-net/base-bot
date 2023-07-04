132db6d8a50154d6ea624a9f1af0638a80d92c84
Jeremy <jeremy@savantly.net>
2023-07-04 16:07:45 -0500

    refactors for generalization
    rename some symbols to be more explicit about the
    operations

--- callback.py
+++ callback.py
@@ -1,4 +1,5 @@
 """Callback handlers used in the app."""
+import logging
 from typing import Any, Dict, List
 
 from langchain.callbacks.base import AsyncCallbackHandler
@@ -17,8 +18,8 @@ class StreamingLLMCallbackHandler(AsyncCallbackHandler):
         await self.websocket.send_json(resp.dict())
 
 
-class QuestionGenCallbackHandler(AsyncCallbackHandler):
-    """Callback handler for question generation."""
+class RephrasedInputGenerationCallbackHandler(AsyncCallbackHandler):
+    """Callback handler for rephrased input generation."""
 
     def __init__(self, websocket):
         self.websocket = websocket
@@ -26,8 +27,9 @@ class QuestionGenCallbackHandler(AsyncCallbackHandler):
     async def on_llm_start(
         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
     ) -> None:
+        logging.info("rephrasing input from prompts %s", prompts)
         """Run when LLM starts running."""
         resp = ChatResponse(
-            sender="bot", message="Synthesizing question...", type="info"
+            sender="bot", message="Rephrasing input...", type="info"
         )
         await self.websocket.send_json(resp.dict())
