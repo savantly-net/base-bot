b19e56d53605a1a076b121d6dddb40f69f72affa
habenkiros <habenhkt@gmail.com>
2023-07-11 19:00:23 +0300

    web scraping using crowl

--- web_scraper/web_scraper/settings.py
+++ web_scraper/web_scraper/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "web_scraper.pipelines.WebScraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "web_scraper.pipelines.WebScraperPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -91,3 +91,10 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+
+FEED_URI = '../data/docs/data.json'
+
+
+FEED_FORMAT = 'json'
+FEED_EXPORT_INDENT = 4
