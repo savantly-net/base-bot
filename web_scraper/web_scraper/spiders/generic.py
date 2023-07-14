acbda31b944c05b1f99b61799dbd694054ab1ae4
habenkiros <habenhkt@gmail.com>
2023-07-14 21:34:59 +0300

    generic parsing

--- web_scraper/web_scraper/spiders/generic.py
+++ web_scraper/web_scraper/spiders/generic.py
@@ -3,6 +3,7 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.linkextractors import LinkExtractor
 from scrapy.settings import Settings
 from scrapy.spiders import CrawlSpider, Spider, Rule
+from scrapy.exporters import JsonLinesItemExporter
 from scrapy.http import Response
 import os
 from twisted.internet.asyncioreactor import install
@@ -22,16 +23,26 @@ class ScrapedPage(scrapy.Item):
     text = scrapy.Field()
     links = scrapy.Field()
 
+
 class GenericSpider(scrapy.Spider):
     name = "generic"
     allowed_domains = ["savantly.net"]
-    start_urls = ["https://savantly.net"]
+
+    custom_settings = {
+        'FEED_FORMAT': 'jsonlines',
+        'FEED_URI': 'output.jsonl'
+    }
+
+    def __init__(self, start_url=None, *args, **kwargs):
+        super(GenericSpider, self).__init__(*args, **kwargs)
+        if start_url:
+            self.start_urls = [start_url]
 
     def parse(self, response):
-        item = {
-            'url': response.url,
-            'status': response.status,
-            'text': '\n'.join(response.xpath('//body//text()').getall()),
-        }
-        yield item
-    
+        scraped_page = ScrapedPage()
+        scraped_page['url'] = response.url
+        scraped_page['status'] = response.status
+        scraped_page['headers'] = response.headers
+        scraped_page['text'] = '\n'.join(response.xpath('//body//text()').getall())
+        scraped_page['links'] = [link.url for link in LinkExtractor(allow_domains=self.allowed_domains).extract_links(response)]
+        yield scraped_page
