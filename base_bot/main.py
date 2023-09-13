b3562fa6ea9004362e7ea0d4354759acc2c88442
Jeremy <jeremy@savantly.net>
2023-09-12 19:44:35 -0500

    adds exception handling

--- base_bot/main.py
+++ base_bot/main.py
@@ -1,9 +1,13 @@
 """Main entrypoint for the app."""
 
+import logging
 import os
+
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from .vectorstore import get_vectorstore
 from .api import router
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 
 
 def create_app() -> FastAPI:
@@ -18,6 +22,14 @@ app = create_app()
 @app.on_event("startup")
 async def startup_event():
     get_vectorstore()
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	logging.error(f"{request}: {exc_str}")
+	content = {'status_code': 10422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 if __name__ == "__main__":
