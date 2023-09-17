e36e151cf14593ec6429e932783e9eeecb220af7
Jeremy <jeremy@savantly.net>
2023-09-17 15:11:15 -0500

    prepare for loader class parameter

--- base_bot/document_loader.py
+++ base_bot/document_loader.py
@@ -1,5 +1,7 @@
 from langchain.document_loaders import DirectoryLoader
 from langchain.document_loaders.base import BaseLoader
+from langchain.document_loaders import PyMuPDFLoader
+#from langchain.document_loaders import UnstructuredFileLoader
 
 
 def get_document_loader(docs_path="data/docs") -> BaseLoader:
@@ -11,5 +13,6 @@ def get_document_loader(docs_path="data/docs") -> BaseLoader:
         show_progress=True,
         use_multithreading=True,
         silent_errors=True,
+#        loader_cls=UnstructuredFileLoader
     )
     return loader
